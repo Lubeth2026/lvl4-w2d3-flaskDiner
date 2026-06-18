a8d9b3df7f05395d9fa3bbeeff3e03aee6354d4d
Lubeth <lubethjimenez@yahoo.com>
2026-06-17 20:32:59 -0400

    Add menu_items function to READ data from the menu.json file created

--- order.py
+++ order.py
@@ -1,2 +1,13 @@
 
-print("Greetings!")
+import json
+
+# print("Greetings!")
+
+ORDERS_FILE = "menu.json"
+
+# Function for loading the menu items READ
+def menu_items():
+    with open(ORDERS_FILE, "r") as file:
+        return json.load(file)
+    
+print(menu_items())
